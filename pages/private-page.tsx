50950f3c17d89088155faf323e552f4c2d62bd98
Alex Devero <deveroalex@gmail.com>
2022-09-27 09:27:26 +0200

    Add login and private page

---
+++ pages/private-page.tsx
@@ -0,0 +1,9 @@
+export default function privatePage() {
+  return (
+    <div>
+      <h1>Private page</h1>
+
+      <p>This is a private page, not accessible for some regions.</p>
+    </div>
+  )
+}
